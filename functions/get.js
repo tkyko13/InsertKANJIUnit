49d5d4d9df1d27b9cbf4b557c4fc012270bd81fb
tkyko13 <>
2021-05-01 12:09:03 +0900

    upd

---
+++ functions/get.js
@@ -0,0 +1,41 @@
+exports.handler = async (event, context) => {
+  // console.log(event.queryStringParameters);
+
+  const kanjiUnit = [
+    '万',
+    '億',
+    '兆',
+    '京',
+    '垓',
+    '𥝱',
+    '穰',
+    '溝',
+    '澗',
+    '正',
+    '載',
+    '極',
+    '恒河沙',
+    '阿僧祇',
+    '那由他',
+    '不可思議',
+    '無量大数'
+  ];
+
+  let numStr = String(event.queryStringParameters.num);
+  let j = numStr.length;
+  for (let i = 0; i < kanjiUnit.length; i++) {
+    j -= 4;
+    if (j <= 0) break;
+
+    numStr = numStr.slice(0, j) + kanjiUnit[i] + numStr.slice(j);
+  }
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'text/html; charset=UTF-8'
+    },
+    body: numStr,
+  };
+};
